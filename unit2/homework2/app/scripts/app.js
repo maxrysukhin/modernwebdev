efe4f31743dbc42577e6b3d3e4830b59a2d5610f
Max Rysukhyn <m.mowgly@gmail.com>
2017-08-16 21:21:52 +0300

    completed HW2

--- unit2/homework2/app/scripts/app.js
+++ unit2/homework2/app/scripts/app.js
@@ -1,5 +1,23 @@
 (function () {
   'use strict';
 
-  angular.module('auction', []);
+     var auction = angular.module('auction', ['ngRoute']);
+
+     auction.config(['$routeProvider',function ($routeProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "views/home.html",
+                controller: 'HomeController',
+                controllerAs: 'ctrl'
+            })
+            .when("/search", {
+                templateUrl: "views/search.html",
+                controller: 'SearchController',
+                controllerAs: 'ctrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }]);
+
 }());
